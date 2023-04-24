421ec10828c9809d8d0ba0cc6ca45850524cdd55
Łukasz Gryglicki <lgryglicki@cncf.io>
2023-04-24 13:16:17 +0000

    WIP
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- check_sync.go
+++ check_sync.go
@@ -14,6 +14,9 @@ import (
 )
 
 func checkSync() (err error) {
+	devstats2landscape := map[string]string{
+		"Foniod": "Fonio",
+	}
 	landscapePath := os.Getenv("LANDSCAPE_YAML_PATH")
 	if landscapePath == "" {
 		landscapePath = "https://raw.githubusercontent.com/cncf/landscape/master/landscape.yml"
@@ -70,14 +73,73 @@ func checkSync() (err error) {
 		fmt.Printf("yaml.Unmarshal '%s' -> %+v", landscapePath, err)
 		return
 	}
-	fmt.Printf("landscape:\n%+v\n", landscape)
 	var projects devstatscode.AllProjects
 	err = yaml.Unmarshal(dataP, &projects)
 	if err != nil {
 		fmt.Printf("yaml.Unmarshal '%s' -> %+v", projectsPath, err)
 		return
 	}
-	fmt.Printf("projects:\n%+v\n", projects)
+	projectsNames := make(map[string]struct{})
+	namesMapping := make(map[string]string)
+	landscapeNames := make(map[string]struct{})
+	joinDatesP := make(map[string]string)
+	joinDatesL := make(map[string]string)
+	for name, data := range projects.Projects {
+		/*
+			MainRepo         string            `yaml:"main_repo"`
+			FullName         string            `yaml:"name"`
+			Status           string            `yaml:"status"`
+			IncubatingDate   *time.Time        `yaml:"incubating_date"`
+			GraduatedDate    *time.Time        `yaml:"graduated_date"`
+			ArchivedDate     *time.Time        `yaml:"archived_date"`
+		*/
+		if data.Disabled {
+			continue
+		}
+		name = strings.ToLower(name)
+		fullName := strings.ToLower(data.FullName)
+		mapped, ok := devstats2landscape[fullName]
+		if ok {
+			fullName = mapped
+		}
+		fullName = strings.ToLower(fullName)
+		projectsNames[fullName] = struct{}{}
+		if name != fullName {
+			namesMapping[name] = fullName
+			namesMapping[fullName] = name
+		}
+		joinDatesP[data.FullName] = data.JoinDate.Format("2006-01-02")
+	}
+	for _, data := range landscape.Landscape {
+		for _, scat := range data.Subcategories {
+			for _, item := range scat.Items {
+				name := strings.ToLower(item.Name)
+				_, ok := projectsNames[name]
+				if !ok {
+					mappedName, okMapped := namesMapping[name]
+					if okMapped {
+						_, ok := projectsNames[mappedName]
+						if ok {
+							name = mappedName
+						}
+					}
+				}
+				if ok {
+					landscapeNames[name] = struct{}{}
+					//joinDatesL[data.FullName] = data.JoinDate.Format("2006-01-02")
+				}
+			}
+		}
+	}
+	fmt.Printf("Projects join dates:\n%+v\n", joinDatesP)
+	fmt.Printf("Landscape join dates:\n%+v\n", joinDatesL)
+	fmt.Printf("Names mapping:\n%+v\n", namesMapping)
+	for name := range projectsNames {
+		_, ok := landscapeNames[name]
+		if !ok {
+			fmt.Printf("error: missing '%s' in landscape\n", name)
+		}
+	}
 	return
 }
 
