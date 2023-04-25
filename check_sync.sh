78d614331d5681304fdd50e44167a4e33d7d857f
Łukasz Gryglicki <lgryglicki@cncf.io>
2023-04-25 07:58:55 +0000

    Add self updating capability, so it will fetch eventual changes automagically
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

---
+++ check_sync.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+date
+lock_file="/tmp/check_sync.lock"
+if [ -f "${lock_file}" ]
+then
+  echo "$0: another check_sync instance is still running, exiting"
+  exit 2
+fi
+if [ -z "$CHECK_SYNC_DIR" ]
+then
+  CHECK_SYNC_DIR="/root/dev/go/src/github.com/cncf/devstats-landscape-sync/"
+fi
+cd "$CHECK_SYNC_DIR" || exit 5
+git pull || exit 6
+make || exit 7
+function cleanup {
+  rm -rf "${lock_file}"
+}
+> "${lock_file}"
+trap cleanup EXIT
+./check_sync 2>&1 | tee -a run.log
